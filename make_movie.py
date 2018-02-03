88ae016adbd147c392bda4e5673f7b3660bd628d
ryanzav <ryan@sensorswitch.com>
2018-02-02 23:28:57 -0500

    reverse output.

--- make_movie.py
+++ make_movie.py
@@ -4,10 +4,19 @@ import os, sys
 
 def combine(target):
     cmd_start = 'ffmpeg -framerate 4 -i '
-    cmd_end = '_%04d.png -c:v libx264 -r 60 -pix_fmt yuv420p out.mp4 -y'
+    cmd_end = '_%04d.png -c:v libx264 -r 60 -pix_fmt yuv420p 1.mp4 -y'
     cmd = cmd_start + target + cmd_end
     print cmd
     os.system(cmd)
+    
+    cmd = 'ffmpeg -i 1.mp4 -vf reverse out.mp4 -y'
+    print cmd
+    os.system(cmd)
+    
+    cmd = 'rm 1.mp4'
+    print cmd
+    os.system(cmd)
+
     return(0)
 
 if __name__ == "__main__":
