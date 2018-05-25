73fec22bcf34c8e51f3caf893f0bfe36471a3e10
ryanzav <ryan@sensorswitch.com>
2018-05-25 11:15:15 -0400

    Update movie file handling to be cross platform.

--- make_movie.py
+++ make_movie.py
@@ -3,28 +3,29 @@
 import os, sys
 
 def combine(folder, target):
-    cmd = 'ffmpeg -framerate 4 -i {folder}{target}_%04d.png -c:v libx264 -r 60 -pix_fmt yuv420p {folder}temp.mp4 -y'
+    temp_file = os.path.join('{folder}','temp.mp4')
+    target_files = os.path.join('{folder}','{target}_%04d.png')
+    output_file = os.path.join('{folder}','out.mp4')
+
+    cmd = 'ffmpeg -framerate 4 -i ' + target_files + ' -c:v libx264 -r 60 -pix_fmt yuv420p ' + temp_file +  ' -y'
     cmd  = cmd.format(folder=folder, target=target)
     print(cmd)
     os.system(cmd)
     
-    cmd = 'ffmpeg -i {folder}temp.mp4 -vf reverse {folder}out.mp4 -y'
+    cmd = 'ffmpeg -i ' + temp_file + ' -vf reverse ' + output_file + ' -y'
     cmd = cmd.format(folder=folder)
     print(cmd)
     os.system(cmd)
     
-    cmd = 'rm {folder}temp.mp4'
-    cmd = cmd.format(folder=folder)
-    print(cmd)
-    os.system(cmd)
+    os.remove(os.path.join(folder,'temp.mp4'))
 
-    cmd = 'rm {folder}{target}*.png'
-    cmd = cmd.format(folder=folder,target=target)
-    print(cmd)
-    os.system(cmd)
+    # cmd = 'rm {folder}/{target}*.png'
+    # cmd = cmd.format(folder=folder,target=target)
+    # print(cmd)
+    # os.system(cmd)
 
     return(0)
 
 if __name__ == "__main__":
-    combine('output/','birdseye')
+    combine('output','birdseye')
 
